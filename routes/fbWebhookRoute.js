831b871271c9677711889a7e2ae91356985872fb
Dienne <minhdien678@gmail.com>
2024-01-30 11:21:49 +0700

    update 30/1

--- routes/fbWebhookRoute.js
+++ routes/fbWebhookRoute.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
   if (mode && token) {
-    if (mode === 'subscribe' && token === process.env.VERIFY_TOEKN) {
+    if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
     } else {
